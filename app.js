5f4e691f21c3292b73f1597a54969dae1f5bdc0d
Pulkit219 <ascension1110@gmail.com>
2017-08-14 00:28:29 -0400

    1st comm

---
+++ app.js
@@ -0,0 +1,16 @@
+var express= require("express");
+var app = express();
+
+app.get("/", function(req,res){
+	res.send("responded");
+
+});
+
+app.get("/bye", function(req,res){
+	res.send("goodbye");
+
+});
+
+app.listen(3000,function(){
+	console.log("server started");
+});

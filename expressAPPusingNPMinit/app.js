3491d4ba7db954965d0d2e5ecbc283327d7177bb
Pulkit219 <ascension1110@gmail.com>
2017-08-17 00:48:28 -0400

    4TH COMM

--- expressAPPusingNPMinit/app.js
+++ expressAPPusingNPMinit/app.js
@@ -6,10 +6,30 @@ app.get("/", function(req,resp){
 });
 
 app.get("/speak/:animal", function(req,resp){
-	var animal = req.params.animal;
-	console.log(animal);
-	  resp.send("Welcome to animals" + animal);
+	var sounds ={
+		pig:"oink",
+		dog:"bark",
+		cat:"meow"
+	}
+	var animal = req.params.animal.toLowerCase();
+	  resp.send("Welcome to animals" + " " + sounds[animal]);
 });
+
+app.get("/repeat/:message/:times", function(req,resp){
+	var message = req.params.message;
+	var times = req.params.times;
+	var result ='';
+
+	for (var i = 0; i < times; i++) {
+		result+=message + '<br/>';
+	}
+  resp.send(result);
+});
+
+app.get("*", function(req,resp){
+  resp.send("PAGE NOT FOUND");
+});
+
 
 
 app.listen(4200, function(){

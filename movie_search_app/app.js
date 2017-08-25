1b106fc2c584149a1be18e423413bcd92b80ec43
Pulkit219 <ascension1110@gmail.com>
2017-08-25 13:41:26 -0400

    10th comm
    added ejs to movieapp

--- movie_search_app/app.js
+++ movie_search_app/app.js
@@ -1,14 +1,15 @@
 var express = require('express');
 var app =express();
 var request = require('request');
+app.set('view engine', 'ejs');
 
 
 app.get('/results', function(req,resp){
-  request('http://www.omdbapi.com/?s=harry&apikey=thewdb',function(error,response,body)
+  request('http://www.omdbapi.com/?s=king&apikey=thewdb',function(error,response,body)
 {
   if(!error && response.statusCode == 200)
-  {var results = JSON.parse(body);
-    resp.send(results['Search'][0]);
+  {var data = JSON.parse(body);
+    resp.render('results', {data:data});
   }
 });
 });

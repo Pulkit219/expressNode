fb56686a9ea6fe7a5207cb38cda301f0c50dfcd5
Pulkit219 <ascension1110@gmail.com>
2017-08-30 15:15:12 -0400

    16th comm

--- NodeExpressYelpCamp/app.js
+++ NodeExpressYelpCamp/app.js
@@ -1,6 +1,16 @@
 var express =require('express');
 var app = express();
 var bodyParser = require("body-parser");
+var campgrounds = [
+   {name: "Salmon Creek", image: "https://farm9.staticflickr.com/8442/7962474612_bf2baf67c0.jpg"},
+   {name: "Granite Hill", image: "https://farm1.staticflickr.com/60/215827008_6489cd30c3.jpg"},
+   {name: "Mountain Goat's Rest", image: "https://farm7.staticflickr.com/6057/6234565071_4d20668bbd.jpg"},
+   {name: "Salmon Creek", image: "https://farm9.staticflickr.com/8442/7962474612_bf2baf67c0.jpg"},
+   {name: "Granite Hill", image: "https://farm1.staticflickr.com/60/215827008_6489cd30c3.jpg"},
+   {name: "Mountain Goat's Rest", image: "https://farm7.staticflickr.com/6057/6234565071_4d20668bbd.jpg"},
+   {name: "Salmon Creek", image: "https://farm9.staticflickr.com/8442/7962474612_bf2baf67c0.jpg"}
+ ];
+
 app.set('view engine' , 'ejs');
 
 app.use(bodyParser.urlencoded({extended:true}));
@@ -11,20 +21,26 @@ app.get('/', function(req, resp)
 });
 
 app.get('/campgrounds', function(req, resp){
-  var campgrounds = [
-     {name: "Salmon Creek", image: "https://farm9.staticflickr.com/8442/7962474612_bf2baf67c0.jpg"},
-     {name: "Granite Hill", image: "https://farm1.staticflickr.com/60/215827008_6489cd30c3.jpg"},
-     {name: "Mountain Goat's Rest", image: "https://farm7.staticflickr.com/6057/6234565071_4d20668bbd.jpg"},
-     {name: "Salmon Creek", image: "https://farm9.staticflickr.com/8442/7962474612_bf2baf67c0.jpg"}
-   ];
+
    resp.render('campgrounds',{campgrounds:campgrounds });
 });
 
 app.get('/campgrounds/new', function(req, resp){
   resp.render('new');
 });
+
+
 app.post('/campgrounds', function(req,resp){
-  resp.send("Post working ");
+   var name = req.body.name;
+   var url= req.body.url;
+   var obj =
+   {
+     name:name,
+     image:url
+   };
+   campgrounds.push(obj);
+
+  resp.redirect("/campgrounds");
 })
 
 app.listen(4200, function()

bace84c9bdca3014aa99a2174c2e8c53083c2e4d
Pulkit219 <ascension1110@gmail.com>
2017-09-06 19:21:26 -0400

    20th comm
    connected to mongoDB success!

--- NodeExpressYelpCamp/app.js
+++ NodeExpressYelpCamp/app.js
@@ -1,6 +1,30 @@
 var express =require('express');
 var app = express();
 var bodyParser = require("body-parser");
+var mongoose = require("mongoose");
+mongoose.Promise = global.Promise;
+mongoose.connect("mongodb://localhost:27017/yelp_camp", {useMongoClient: true});;
+
+//SCHEMA
+var campgroundschema = new mongoose.Schema({
+    name: String,
+    image: String
+});
+
+var Campground = mongoose.model('Campground',campgroundschema );
+// Campground.create(
+//    {name: "Granite Hill", image: "https://farm1.staticflickr.com/60/215827008_6489cd30c3.jpg"},
+// function(err,campground){
+//   if(err)
+//   console.log(err);
+//
+//   else {
+//     console.log('created!!!');
+//     console.log(campground);
+//   }
+// }
+// );
+
 var campgrounds = [
    {name: "Salmon Creek", image: "https://farm9.staticflickr.com/8442/7962474612_bf2baf67c0.jpg"},
    {name: "Granite Hill", image: "https://farm1.staticflickr.com/60/215827008_6489cd30c3.jpg"},
@@ -10,6 +34,7 @@ var campgrounds = [
    {name: "Mountain Goat's Rest", image: "https://farm7.staticflickr.com/6057/6234565071_4d20668bbd.jpg"},
    {name: "Salmon Creek", image: "https://farm9.staticflickr.com/8442/7962474612_bf2baf67c0.jpg"}
  ];
+
 
 app.set('view engine' , 'ejs');
 
@@ -26,7 +51,17 @@ app.get('/', function(req, resp)
 
 app.get('/campgrounds', function(req, resp){
 
-   resp.render('campgrounds',{campgrounds:campgrounds });
+  //  resp.render('campgrounds',{campgrounds:campgrounds });
+  Campground.find({},
+  function(err,allcampground){
+    if(err)
+    console.log(err);
+
+    else {
+    resp.render('campgrounds',{campgrounds:allcampground });
+    }
+  }
+  );
 });
 
 

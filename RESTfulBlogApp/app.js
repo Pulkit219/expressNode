190e252506e03abc9e9147020f329b47f5119c15
Pulkit219 <ascension1110@gmail.com>
2017-09-09 12:00:56 -0400

    25th comm

--- RESTfulBlogApp/app.js
+++ RESTfulBlogApp/app.js
@@ -3,5 +3,24 @@ var app = express();
 var bodyParser = require("body-parser");
 var mongoose = require("mongoose");
 
-mongoose.Promise = global.Promise;
+// mongoose.Promise = global.Promise;
 mongoose.connect("mongodb://localhost:27017/restful_blog_app", {useMongoClient: true});
+
+app.set('view engine' , 'ejs');
+app.use(bodyParser.urlencoded({extended:true}));
+app.use(express.static('public'));
+
+
+var blogschema = new mongoose.Schema({
+    title: String,
+    image: String,
+    body:String,
+    created:{type:Date,default:Date.now}
+});
+
+var Blog = mongoose.model('Blog', blogschema)
+
+app.listen(4200, function()
+{
+  console.log('server started');
+})

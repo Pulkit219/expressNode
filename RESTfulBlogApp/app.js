c9136cbbabcf9d780de9c788a191c80f5f8f7d5b
Pulkit219 <ascension1110@gmail.com>
2017-09-10 23:44:51 -0400

    Revert "th comm"
    This reverts commit 48d9261bd76fd72b08a61b852226d8963c665791.

--- RESTfulBlogApp/app.js
+++ RESTfulBlogApp/app.js
@@ -6,9 +6,9 @@ var mongoose = require("mongoose");
 // mongoose.Promise = global.Promise;
 mongoose.connect("mongodb://localhost:27017/restful_blog_app", {useMongoClient: true});
 
-// app.set('view engine' , 'ejs');
-// app.use(bodyParser.urlencoded({extended:true}));
-// app.use(express.static('public'));
+app.set('view engine' , 'ejs');
+app.use(bodyParser.urlencoded({extended:true}));
+app.use(express.static('public'));
 
 
 var blogschema = new mongoose.Schema({
@@ -18,11 +18,7 @@ var blogschema = new mongoose.Schema({
     created:{type:Date,default:Date.now}
 });
 
-var Blog = mongoose.model('Blog', blogschema);
-
-//RESTFUL ROUTES
-
-
+var Blog = mongoose.model('Blog', blogschema)
 
 app.listen(4200, function()
 {

b0ec2e7d6689ab1946930ec9c59270bd6f3aa585
Pulkit219 <ascension1110@gmail.com>
2017-09-21 13:26:29 -0400

    43rd comm
    adding models

--- Associations/models/user.js
+++ Associations/models/user.js
@@ -1,9 +1,4 @@
-var Post = require('./models/post');
 var mongoose = require('mongoose');
-mongoose.connect("mongodb://localhost:27017/blog_demo_2", {useMongoClient: true});
-
-
-//USER SCHEMA
 var userSchema = new mongoose.Schema({
   email:String,
   name:String,
@@ -14,3 +9,5 @@ var userSchema = new mongoose.Schema({
     }
   ]
 });
+
+module.exports = mongoose.model('User',userSchema);

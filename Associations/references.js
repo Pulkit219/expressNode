0dda1246ebbf82a75d35084f5301de922d0a78e0
Pulkit219 <ascension1110@gmail.com>
2017-09-21 14:34:53 -0400

    44th comm

--- Associations/references.js
+++ Associations/references.js
@@ -1,16 +1,48 @@
-var Post = require('./models/post');
 var mongoose = require('mongoose');
 mongoose.connect("mongodb://localhost:27017/blog_demo_2", {useMongoClient: true});
+var Post = require('./models/post');
+var User = require('./models/user');
 
+// User.create(
+//   {
+//     email:'pulkit@gmail.com',
+//     name:'Pulkit'
+//   }
+// );
+// Post.create(
+//   {
+//     title:'Testing 3 ',
+//     content:'Testing 3Testing 3Testing 3Testing 3Testing 3Testing 3Testing 3Testing 3'
+//   },function(err,post)
+//   {
+//     if(err)
+//      console.log(err);
+//      else {
+//        User.findOne({name:'Pulkit'},function(err,userFound){
+//          if(err)
+//           console.log(err);
+//           else {
+//             {
+//               userFound.posts.push(post);
+//               userFound.save(function(err,data){
+//                 if(err)
+//                 console.log(err);
+//                 else{
+//                   console.log(data);
+//                 }
+//               })
+//             }
+//           }
+//        })
+//      }
+//   }
+// );
 
-//USER SCHEMA
-var userSchema = new mongoose.Schema({
-  email:String,
-  name:String,
-  posts:[
-    {
-      type:mongoose.Schema.Types.ObjectId,
-      ref:'Post'
-    }
-  ]
-});
+User.findOne({name:'Pulkit'}).populate('posts').exec(function(err,user){
+  if(err){
+    console.log(err);
+  }
+  else{
+    console.log(user);
+  }
+})

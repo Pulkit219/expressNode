70891dc823310b7a62b7f8eab7ef179f40ca8f00
Pulkit219 <ascension1110@gmail.com>
2017-09-21 02:01:03 -0400

    42nd comm
    nested objects , one to many relationship

--- Associations/embed.js
+++ Associations/embed.js
@@ -1,14 +1,6 @@
 var mongoose = require('mongoose');
 mongoose.connect("mongodb://localhost:27017/blog_demo", {useMongoClient: true});
 
-//USER SCHEMA
-var userSchema = new mongoose.Schema({
-  email:String,
-  name:String
-});
-
-var User = mongoose.model('User', userSchema);
-
 //POST Schema
 var postSchema = new mongoose.Schema({
   title:String,
@@ -16,13 +8,64 @@ var postSchema = new mongoose.Schema({
 });
 
 var Post = mongoose.model('Post', postSchema);
+//USER SCHEMA
+var userSchema = new mongoose.Schema({
+  email:String,
+  name:String,
+  posts:[postSchema]
+});
+
+var User = mongoose.model('User', userSchema);
+
+//RETRIVE EXISTING User
+User.findOne({name:'Pulkit'}, function(err,user)
+{
+  if(err)
+   console.log(err);
+   else {
+  user.posts.push({
+    title:'how to code pt2',
+    content:'Just focus'
+  });
+  user.save(function(err,user){
+      if(err)
+      console.log(err);
+      else {
+      console.log(user);
+
+      }
+  })
+
+   }
+})
 
 // var newUser = new User({
-//   email:'johndoe@gmail.com',
-//   name:'John Doe'
+//   email:'pulkit@gmail.com',
+//   name:'Pulkit'
 // });
 //
-// newUser.save(function(err,post){
+// newUser.posts.push({
+//   title:"how to code",
+//   content:'just focus and write'
+// })
+//
+// newUser.save(function(err,user){
+//
+//   if(err)
+//   console.log(err);
+//   else {
+//   console.log(user);
+//
+//   }
+// });
+
+
+// var newPost = new Post({
+//   title:'Reflections on app',
+//   name:'testing'
+// });
+//
+// newPost.save(function(err,post){
 //
 //   if(err)
 //   console.log(err);
@@ -31,19 +74,3 @@ var Post = mongoose.model('Post', postSchema);
 //
 //   }
 // });
-
-
-var newPost = new Post({
-  title:'Reflections on app',
-  name:'testing'
-});
-
-newPost.save(function(err,post){
-
-  if(err)
-  console.log(err);
-  else {
-  console.log(post);
-
-  }
-});

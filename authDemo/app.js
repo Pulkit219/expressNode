4d77b6fe07bd23de8496fb0bfbef4dd21d5b153f
Pulkit219 <ascension1110@gmail.com>
2017-09-29 13:41:38 -0400

    58th comm
    express session , passport , plugin added to User model

--- authDemo/app.js
+++ authDemo/app.js
@@ -2,11 +2,25 @@ var express =require('express');
 var app = express();
 var bodyParser = require("body-parser");
 var mongoose = require("mongoose");
+var passport = require('passport');
+var localStrategy = require('passport-local');
+var passportLocalMongoose = require('passport-local-mongoose');
+var User = require ('./models/user');
 
 
 mongoose.Promise = global.Promise;
 mongoose.connect("mongodb://localhost:27017/auth_app", {useMongoClient: true});
 app.set('view engine' , 'ejs');
+app.use(require('express-session')({
+  secret:'SOme secret code',
+  resave:false,
+  saveUninitialized:false
+}));
+app.use(passport.initialize());
+app.use(passport.session());
+
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
 app.use(bodyParser.urlencoded({extended:true}));
 
 
@@ -20,6 +34,10 @@ app.get('/', function(req, resp)
   resp.render("home");
 });
 
+app.get('/secret', function(req, resp)
+{
+  resp.render("secret");
+});
 
 
 app.listen(4200, function()

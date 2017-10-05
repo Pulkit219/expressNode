61f8cc67fdb495df06a8d247fd7e48b26f32035a
Pulkit219 <ascension1110@gmail.com>
2017-10-04 20:00:07 -0400

    66th comm
    passport configuration done

--- NodeExpressYelpCamp/app.js
+++ NodeExpressYelpCamp/app.js
@@ -13,6 +13,22 @@ var seedDB    = require('./seeds');
 mongoose.Promise = global.Promise;
 mongoose.connect("mongodb://localhost:27017/yelp_camp", {useMongoClient: true});
 seedDB();
+
+//PASSPORT CONFIGURATION
+//===========================================
+app.use(require('express-session')({
+  secret:'SOme secret code',
+  resave:false,
+  saveUninitialized:false
+}));
+app.use(passport.initialize());
+app.use(passport.session());
+
+passport.use(new LocalStrategy(User.authenticate()));
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+//==========================================================
+
 app.set('view engine' , 'ejs');
 app.use(express.static(__dirname  + '/public'));
 app.use(bodyParser.urlencoded({extended:true}));
--- NodeExpressYelpCamp/models/user.js
+++ NodeExpressYelpCamp/models/user.js
@@ -1,9 +1,10 @@
 //SCHEMA
 var mongoose = require("mongoose");
+var passportLocalMongoose = require('passport-local-mongoose');
 var UserSchema = new mongoose.Schema({
     name: String,
     password: String
 
 });
-
+UserSchema.plugin(passportLocalMongoose);
 module.exports =mongoose.model('User',UserSchema );
